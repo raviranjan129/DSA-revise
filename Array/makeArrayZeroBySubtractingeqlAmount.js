fbab1ed0e2315cf2f6aa2affb0def8f99c92ad51
name <rk6318633@gmail.com>
2025-03-23 14:58:11 +0530

    makeArrayZeroBYSubtractingEqlAmount

---
+++ Array/makeArrayZeroBySubtractingeqlAmount.js
@@ -0,0 +1,18 @@
+
+function makeElementZero(arr){
+
+    let unique=new Set();  //set method is used to store element with unique value;
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i] !=0){              //we are checking if zero then it should not present in the unique set;
+            unique.add(arr[i]);         
+        }
+    }
+    return unique.size;
+}
+
+let arr=[1,2,3,4,4,5,0];
+
+
+
+console.log(makeElementZero(arr));
