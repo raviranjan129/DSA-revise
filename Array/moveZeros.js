5b403e13d676035ace077c20650725f65ed9ddb1
name <rk6318633@gmail.com>
2025-05-08 16:13:37 +0530

    moveZeros

---
+++ Array/moveZeros.js
@@ -0,0 +1,17 @@
+
+function moveZeros(arr){
+    let i=0;
+
+    for(let j=1;j<arr.length;j++){
+        if(arr[i] ==0 && arr[j] !=0){
+            [arr[i],arr[j]]=[arr[j],arr[i]];
+
+            i++;
+        }
+    }
+    return arr;
+}
+
+let arr=[0,1,0,3,12];
+
+console.log(moveZeros(arr))
